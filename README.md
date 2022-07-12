2de9eb841230ba23893a96d0ac028ffdfa3605bf
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-07-12 22:02:51 +0530

    tests

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ASP.Net-AL-BE-Project-2-EF-Part-A-Initial
+####  CREATE MIGRATIONS
+```
+dotnet ef migrations add MigrationV1 --project Student-API.csproj
+
+```
+
+
+#### List Unapplied Migrations
+```
+ dotnet ef migrations  list --project Student-API.csproj
+
+```
+#### Reflect Migrations in Db
+```
+dotnet ef database update MigrationV1 --project Student-API.csproj
+
+``` 
+
+
+
+####  DOCKER-POSTGRES CONTAINER CREATE/UP
+```
+docker-compose up
+```
---
+++ UnitTests/Controllers/StudentControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Student_API.Controllers;
+using Student_API.Domain;
+using Student_API.Models;
+using Student_API.Services.Interface;
+
+namespace UnitTests.Controllers;
+
+public class StudentControllerTest
+{
+    [Fact]
+    public void ShouldReturn200OnSuccess()
+    {
+        var mockStudentService = new Mock<IStudentService>();
+        mockStudentService.Setup(service => service.GetAllStudents()).Returns(new List<Student>());
+        var sut = new StudentController(mockStudentService.Object);
+
+        var result = sut.GetAll();
+        var res = (OkObjectResult)result.Result;
+        var objRes = (StudentResponse)res.Value;
+
+        Assert.Equal(200, res.StatusCode);
+        Assert.Null(objRes.Error);
+        Assert.NotNull(objRes.Data);
+
+        mockStudentService.Verify(service => service.GetAllStudents(), Times.Once);
+    }
+
+
+
+    [Fact]
+    public void ShouldReturn404NotFoundWhenExceptionOccurred()
+    {
+        var mockStudentService = new Mock<IStudentService>();
+        mockStudentService.Setup(service => service.GetAllStudents()).Throws(new Exception("Server Error"));
+        var sut = new StudentController(mockStudentService.Object);
+
+        var result = sut.GetAll();
+        var res = (NotFoundObjectResult)result.Result;
+        var objRes = (StudentResponse)res.Value;
+
+        Assert.Equal(404, res.StatusCode);
+        Assert.Equal("Server Error", objRes.Error);
+        Assert.Null(objRes.Data);
+
+        mockStudentService.Verify(service => service.GetAllStudents(), Times.Once);
+    }
+}
